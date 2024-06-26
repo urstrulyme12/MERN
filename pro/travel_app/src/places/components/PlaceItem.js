43b5d3fefcc6365e68c7b9a555eda7b1f5761c80
urstrulyme12 <105495125+urstrulyme12@users.noreply.github.com>
2024-06-26 15:58:32 +0530

    changes

---
+++ pro/travel_app/src/places/components/PlaceItem.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./PlaceItem.css"
+import Card from '../../shared/UIElements/Card'
+
+function PlaceItem(props) {
+  return (
+    <>
+    <li className='place-item'>
+        <div className='place-item__image'>
+            <img src={props.image} alt={props.title}/>
+
+        </div>
+        <div className='place-item__info'>
+            <h2>{props.title}</h2>
+            <h3>{props.address}</h3>
+            <p>{props.description}</p>
+        </div>
+        <div className='place-item__actions'>
+    <button>VIEW ON MAP</button>
+    <button>EDIT</button>
+    <button>Delete</button>
+        </div>
+
+    </li>
+    </>
+  )
+}
+
+export default PlaceItem
---
+++ pro/travel_app/src/places/components/PlaceList.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Card from '../../shared/UIElements/Card'
+import "./PlaceList.css"
+import PlaceItem from  './PlaceItem' 
+
+function PlaceList(props) {
+    if(props.items.length ===0){
+        return <div className='place-list center'>
+            <Card>
+                <h2>NO Places Found ! May be create one?</h2>
+                <button> Share Place</button>
+            </Card>
+        </div>
+    }
+  return (
+   <>
+   <ul className='place-list'>
+{props.items.map((place)=>{
+    return <PlaceItem  key={place.id} id={place.id} image={place.imageUrl} title={place.title} description = {place.description} address={place.address} creatorId = {place.creator} coordinates={place.location} />
+})}
+   </ul>
+
+   </>
+  )
+}
+
+export default PlaceList

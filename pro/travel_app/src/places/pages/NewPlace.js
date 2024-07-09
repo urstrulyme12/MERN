59e6e414ac2ef9fcb82d71e5b97d3f907a36cc86
urstrulyme12 <105495125+urstrulyme12@users.noreply.github.com>
2024-07-09 18:45:11 +0530

    validation

--- pro/travel_app/src/places/pages/NewPlace.js
+++ pro/travel_app/src/places/pages/NewPlace.js
@@ -1,16 +1,22 @@
-import React from 'react'
-import "./NewPlace.css"
-import Input from '../../shared/FormElements/Input'
+import React from "react";
+import "./NewPlace.css";
+import Input from "../../shared/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
 
-const NewPlace=(props)=> {
+const NewPlace = (props) => {
   return (
-   <>
-        <form className='place-form'>
-         <Input element="input" type="text" label="Title" />
+    <>
+      <form className="place-form">
+        <Input
+          element="input"
+          type="text"
+          label="Title"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Pls enter valid text"
+        />
+      </form>
+    </>
+  );
+};
 
-        </form>
-   </>
-  )
-}
-
-export default NewPlace
+export default NewPlace;
--- pro/travel_app/src/shared/FormElements/Input.js
+++ pro/travel_app/src/shared/FormElements/Input.js
@@ -1,27 +1,74 @@
-import React from "react";
-import "./Input.css"
-import { useState } from "react";
+import React, { useReducer } from "react";
+import "./Input.css";
+import { validate } from "../util/validators";
+
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE":
+      return {
+        ...state,
+        value: action.val,
+        isValid: validate(action.val, action.validators),
+      };
+      case "TOUCH":
+        return {
+          ...state,
+          isTouched: true,
+        };
+    default:
+      return state;
+  }
+};
 
 function Input(props) {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: "",
+    isValid: false,
+    isTouched: false,
+  });
+  const changeHandler = (event) => {
+    dispatch({
+      type: "CHANGE",
+      val: event.target.value,
+      validators: props.validators,
+    });
+  };
 
-    const changeHandler=(event)=>{
-        
-
-    }
+  const touchHandler=()=>{
+    dispatch({
+      type: "TOUCH",
+    });
+  }
 
   const element =
-  
     props.element === "input" ? (
-      <input is={props.id} type={props.type} placeholder={props.placeholder} onChange={changeHandler} />
+      <input
+        is={props.id}
+        type={props.type}
+        placeholder={props.placeholder}
+        onChange={changeHandler}
+        onBlur={touchHandler}
+        value={inputState.value}
+      />
     ) : (
-      <textarea id={props.id} rows={props.rows || 3} onChange={changeHandler} />
+      <textarea
+        id={props.id}
+        rows={props.rows || 3}
+        onChange={changeHandler}
+        onBlur={touchHandler}
+        value={inputState.value}
+      />
     );
 
-   
   return (
-    <div className={`form-control`}>
+    <div
+      className={`form-control ${
+        !inputState.isValid && inputState.isTouched && "form-control--invalid"
+      }`}
+    >
       <label htmlFor={props.id}>{props.label}</label>
       {element}
+      {!inputState.isValid && inputState.isTouched &&<p>{props.errorText}</p>}
     </div>
   );
 }
